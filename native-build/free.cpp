f432496832716d3c6408ee9727cca1911afa9529
Zun <contact@colonysurvival.nl>
2026-09-18 14:58:48 +0200

    Match Windows export linkage for bytecode buffer cleanup

--- native-build/free.cpp
+++ native-build/free.cpp
@@ -1,13 +1,7 @@
 #include "luau_native.h"
 #include <cstdlib>
 
-#if defined(_WIN32)
-#define NATIVE_EXPORT __declspec(dllexport)
-#else
-#define NATIVE_EXPORT __attribute__((visibility("default")))
-#endif
-
-extern "C" NATIVE_EXPORT void luau_native_free(void* buffer)
+void luau_native_free(void* buffer)
 {
     std::free(buffer);
 }
--- native-build/luau_native.h
+++ native-build/luau_native.h
@@ -1,13 +1,23 @@
 #pragma once
 #include <stddef.h>
 
+#if defined(_WIN32)
+#if defined(LUAU_NATIVE_BUILD)
+#define LUAU_NATIVE_API __declspec(dllexport)
+#else
+#define LUAU_NATIVE_API __declspec(dllimport)
+#endif
+#else
+#define LUAU_NATIVE_API __attribute__((visibility("default")))
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // Free the buffer returned by luau_compile in the same native library that
 // allocated it. Never use Marshal.FreeHGlobal or a different CRT's free().
-void luau_native_free(void* buffer);
+LUAU_NATIVE_API void luau_native_free(void* buffer);
 
 #ifdef __cplusplus
 }
